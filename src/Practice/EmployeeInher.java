a8d1a43df24cc0eb19bf752f0b42140593513be9
pyada <pyada@10.0.0.56>
2022-02-17 13:14:03 -0500

    Done

--- src/Practice/EmployeeInher.java
+++ src/Practice/EmployeeInher.java
@@ -3,7 +3,8 @@ package Practice;
 public class EmployeeInher {
 	public static void main(String args[]){  
 		   Programmer p=new Programmer();  
-		   System.out.println("Programmer salary is:"+p.salary);  
+		   System.out.println("Programmer salary is:"+p.salary);
+		   System.out.println("Bonus salary");
 		   System.out.println("Bonus of Programmer is:"+p.bonus);  
 		}  
 
